8368106814e8e02c00136af11cf1812810d80125
Jz1116 <vjiang1116@outlook.com>
2020-08-11 22:32:40 -0400

    Move books across shelves

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 import SearchPage from "./components/SearchPage/SearchPage";
 import MainPage from "./components/MainPage/MainPage";
 
 class BooksApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      booksOnShelf: [],
+    };
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      const bookData = books.map((book) => {
+        return {
+          id: book.id,
+          title: book.title,
+          author: book.authors.join(", "),
+          thumbnail: book.imageLinks.thumbnail,
+          shelf: book.shelf,
+        };
+      });
+      this.setState({
+        booksOnShelf: bookData,
+      });
+    });
+  }
+
+  updateBookData = async (id, shelf) => {
+    await BooksAPI.update(id, shelf);
+    const { booksOnShelf } = this.state;
+
+    booksOnShelf.forEach((book) => {
+      if (book.id === id) {
+        book.shelf = shelf;
+      }
+    });
+
+    this.setState({
+      booksOnShelf,
+    });
+  };
+
   render() {
+    const { booksOnShelf } = this.state;
+
     return (
       <div className="app">
         <Switch>
@@ -13,7 +56,10 @@ class BooksApp extends React.Component {
             <SearchPage />
           </Route>
           <Route path="/">
-            <MainPage />
+            <MainPage
+              booksOnShelf={booksOnShelf}
+              updateBookData={this.updateBookData}
+            />
           </Route>
         </Switch>
       </div>
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,7 +6,31 @@ import WantToRead from "./components/WantToRead";
 import Read from "./components/Read";
 
 class MainPage extends Component {
+  getBooksOnShelves = (books) => {
+    const currentlyReading = [];
+    const wantToRead = [];
+    const read = [];
+
+    books.forEach((book) => {
+      if (book.shelf === "currentlyReading") {
+        currentlyReading.push(book);
+      } else if (book.shelf === "wantToRead") {
+        wantToRead.push(book);
+      } else {
+        read.push(book);
+      }
+    });
+
+    return { currentlyReading, wantToRead, read };
+  };
+
   render() {
+    const { booksOnShelf, updateBookData } = this.props;
+
+    const { currentlyReading, wantToRead, read } = this.getBooksOnShelves(
+      booksOnShelf
+    );
+
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -14,9 +38,15 @@ class MainPage extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <CurrentReading />
-            <WantToRead />
-            <Read />
+            <CurrentReading
+              currentlyReading={currentlyReading}
+              updateBookData={updateBookData}
+            />
+            <WantToRead
+              wantToRead={wantToRead}
+              updateBookData={updateBookData}
+            />
+            <Read read={read} updateBookData={updateBookData} />
           </div>
         </div>
         <div className="open-search">
